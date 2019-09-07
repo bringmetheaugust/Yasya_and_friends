d5969df02e6332858a677e92a259b65839615c71
bringmetheaugust <bringmetheaugust@gmail.com>
2019-09-07 22:10:52 +0300

    add method to parse secret words

--- src/component/Menu/index.jsx
+++ src/component/Menu/index.jsx
@@ -3,10 +3,31 @@ import {Link} from "react-router-dom";
 
 import heroes from '@src/constant/heroes.js';
 import css from './index.module.sass';
+import * as SECRET_WORDS from '@src/constant/secretWords.js';
 
 const MENU_AUDIO = require("@src/media/menu/menu.mp3");
 
 export default class Menu extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { secretWord: '' };
+	}
+	checkSecretWord = ({ key }) => {
+		const completedLetters = this.state.secretWord + key;
+		switch (true) {
+			case (completedLetters.includes(SECRET_WORDS.NATASHA_SECRET_WORD)): {
+				this.setState({ secretWord: '' });
+				return;
+			}
+			case (completedLetters.includes(SECRET_WORDS.VITYA_SECTER_WORD)): {
+				this.setState({ secretWord: '' });
+				return;
+			}
+		}
+		this.setState({ secretWord: completedLetters});
+	}
+	componentDidMount = () => addEventListener('keydown', this.checkSecretWord);
+	componentWillUnmount =() => removeEventListener('keydown', this.checkSecretWord);
 	render() {
 		return(
 			<section className={css.menu}>
---
+++ src/constant/secretWords.js
@@ -0,0 +1,2 @@
+export const NATASHA_SECRET_WORD = 'abubabu';
+export const VITYA_SECTER_WORD = 'angular';
